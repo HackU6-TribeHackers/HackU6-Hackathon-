0692b4b9dec569cafbf7f958439f435f4a31e90f
Cory Thomas <corythomas@Corys-MacBook-Air.local>
2018-02-23 12:58:09 -0500

    have frontend calling backend to get tweets; need to now display the tweets on the final page

--- homefind/app/views.py
+++ homefind/app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+from twitter_data_receiving.main import mainReceiver
 
 
 def index(request):
@@ -19,6 +20,9 @@ def name(request):
         # create a form instance and populate it with data from the request:
         zip = request.POST.get('zip', None)
         radius = request.POST.get('radius', None)
+        tweets, users = mainReceiver(zip, radius)
+        # for i in range(5):
+        #     print("%s : %s" % (users[i], tweets[i]))
         # check whether it's valid:
         # process the data in form.cleaned_data as required
         # ...
