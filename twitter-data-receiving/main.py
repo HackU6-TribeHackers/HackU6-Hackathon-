a72409e1d40ecba6491e13e431303079e335f9e6
Cory Thomas <corythomas@Corys-MacBook-Air.local>
2018-02-22 20:34:37 -0500

    moved geocode generation to a function in seperate file

--- twitter-data-receiving/main.py
+++ twitter-data-receiving/main.py
@@ -1,8 +1,8 @@
 import requests
 import json
 import tweepy
-import googlemaps
 from twitter_data import getTwitterData
+from geocoding import getGeocode
 
 
 CITY_NEEDED = [0, 7, 10, 11, 12, 14]
@@ -19,7 +19,7 @@ with open("requirements/key-phrases.txt", "r") as phrases_file:
 with open("../api-keys.json", "r") as api_keys:
 	api_keys_json = json.load(api_keys)
 
-# google authentication keys
+# google authentication key
 GOOGLE_KEY = api_keys_json['google_api_key']
 
 # all of the twitter authentication keys
@@ -28,23 +28,7 @@ CONSUMER_SECRET = api_keys_json['twitter_consumer_secret']
 ACCESS_TOKEN = api_keys_json['twitter_access_token']
 ACCESS_SECRET = api_keys_json['twitter_access_secret']
 
-location = input("Where are you currently located : ")
-radius = input("How many miles of a radius would you like to search : ")
-print ()
-
-gmaps = googlemaps.Client(key=GOOGLE_KEY)
-
-geocode = gmaps.geocode(location)
-
-formatted_address = geocode[0]['formatted_address'].split(',')
-target_city = formatted_address[len(formatted_address) - 3]
-
-coordinates = geocode[0]['geometry']['location']
-latitude = str(coordinates['lat'])
-longitude = str(coordinates['lng'])
-
-geocode_string = latitude + ',' + longitude + ',' + radius + 'mi'
-
+geocode_string, target_city = getGeocode(GOOGLE_KEY)
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
